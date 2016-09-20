94b4b01297330426abcb156560f6f3bc316404cc
Shi Tuocheng <shituocheng@126.com>
2016-09-20 18:17:38 +0800

    add other functions

---
+++ script/about.js
@@ -0,0 +1,38 @@
+/**
+ * Created by shituocheng on 2016/9/20.
+ */
+function showSection(id) {
+    var sections = document.getElementsByTagName("section");
+    for (var i=0; i<sections.length; i++ ) {
+        if (sections[i].getAttribute("id") != id) {
+            sections[i].style.display = "none";
+        } else {
+            sections[i].style.display = "block";
+        }
+    }
+}
+
+function prepareInternalnav() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    var articles = document.getElementsByTagName("article");
+    if (articles.length == 0) return false;
+    var navs = articles[0].getElementsByTagName("nav");
+    if (navs.length == 0) return false;
+    var nav = navs[0];
+    var links = nav.getElementsByTagName("a");
+    for (var i=0; i<links.length; i++ ) {
+        var sectionId = links[i].getAttribute("href").split("#")[1];
+        if (!document.getElementById(sectionId)) continue;
+        document.getElementById(sectionId).style.display = "none";
+        links[i].destination = sectionId;
+        links[i].onclick = function() {
+            showSection(this.destination);
+            return false;
+        }
+    }
+}
+
+window.onload = function () {
+    prepareInternalnav();
+}
